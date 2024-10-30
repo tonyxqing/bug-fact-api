c34cc338a728a6043da4002a34f787da449a8134
tonyxqing <tonyqing2022@gmail.com>
2024-10-30 18:07:21 -0500

    initializing spring boot project

---
+++ src/main/java/com/stub/rest_sqlite/RestSqliteApplication.java
@@ -0,0 +1,13 @@
+package com.stub.rest_sqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSqliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSqliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stub/rest_sqlite/RestSqliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stub.rest_sqlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSqliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
